f6db32091a05f9b7683d55275ee40aeaeb6f8f32
zurira <20243ds036@utez.edu.mx>
2026-03-07 15:43:47 -0600

    SE implemento la clase JwtFilter como filtro de seguridad

---
+++ backend/src/main/java/com/sigpro/security/JwtFilter.java
@@ -0,0 +1,52 @@
+package com.sigpro.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        // busca el encabezado llamado Authorization
+        final String authHeader = request.getHeader("Authorization");
+        String matricula = null;
+        String jwt = null;
+
+        // verifica que el encabezado exista
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            jwt = authHeader.substring(7);
+            try {
+                //obtiene la matricula desde el token
+                matricula = jwtUtil.extractMatricula(jwt);
+            } catch (Exception e){
+                logger.error("Token inválido o expirado");
+            }
+        }
+
+        if (matricula != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // verifica que el token sea valido
+            if (jwtUtil.validarToken(jwt, matricula)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        matricula, null, new ArrayList<>());
+                // permite al usuario ver las rutas protegidas
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        // permite que la petición llegue al controlador
+        chain.doFilter(request, response);
+
+    }
+}
