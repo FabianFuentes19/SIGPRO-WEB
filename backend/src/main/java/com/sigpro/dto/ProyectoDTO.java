86472f5a71d124ac2da940cc7d8289ec1748315b
zurira <20243ds036@utez.edu.mx>
2026-03-12 23:27:39 -0600

    Implementación de la clase ProyectoDTO

---
+++ backend/src/main/java/com/sigpro/dto/ProyectoDTO.java
@@ -0,0 +1,18 @@
+package com.sigpro.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class ProyectoDTO {
+    private String nombre;
+    private String descripcion;
+    private String objetivoGeneral;
+    private Long liderId;
+    private BigDecimal presupuesto;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private String estado;
+}
