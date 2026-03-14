05eb83e6bf9873753b2251e3ff19b6a6091477cf
zurira <20243ds036@utez.edu.mx>
2026-03-14 13:57:47 -0600

    Implementación del servico de email, para el reestablecimiento de contraseña

---
+++ backend/src/main/java/com/sigpro/service/EmailService.java
@@ -0,0 +1,42 @@
+package com.sigpro.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    // permite la conexión con SMTP
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+    public void enviarCorreoRecuperacion(String matricula, String otpToken) {
+        // se crea el correo con la matricula del estudiante + @utez.edu.mx
+        String correoDestino = matricula.toLowerCase() + "@utez.edu.mx";
+
+        // objeto que permite construir el correo
+        SimpleMailMessage mensaje = new SimpleMailMessage();
+        mensaje.setFrom(senderEmail); // quien lo envía
+        mensaje.setTo(correoDestino); // a quien se le envía
+        mensaje.setSubject("SIGPRO - Recuperación de Contraseña");
+
+        // creación del cuerpo del correo
+        mensaje.setText("Hola " + matricula + ",\n\n"
+                + "Se ha solicitado un restablecimiento de contraseña para tu cuenta en SIGPRO.\n\n"
+                + "Tu código temporal de 6 dígitos es: " + otpToken + "\n\n"
+                + "Este código expirará en 5 minutos.\n"
+                + "Si no solicitaste esto, ignora este correo.\n\n"
+                + "Atentamente,\nEl Equipo SIGPRO");
+
+        // detona el envío del correo
+        mailSender.send(mensaje);
+
+        System.out.println("Correo enviado exitosamente a: " + correoDestino);
+    }
+}
