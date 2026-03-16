3d182e88081555057093a8e080c553162d4ad09d
TaniaSR8 <20243ds011@utez.edu.mx>
2026-03-15 19:34:19 -0600

    Modifique las rutas temporales

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './components/Login'
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Proyectos from "./components/Proyectos";
+import AdminRoute from "./components/AdminRoute";
 
+function App() {
   return (
-    <>
-      <div>
-      <Login/>
-      </div>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        {/* Ruta protegida */}
+        <Route path="/proyectos" element={
+          <AdminRoute>
+            <Proyectos />
+          </AdminRoute>
+        } />
+
+        <Route path="/dashboard" element={<h2>Dashboard</h2>} />
+      </Routes>
+    </Router>
+  );
 }
 
-
-export default App
+export default App;
