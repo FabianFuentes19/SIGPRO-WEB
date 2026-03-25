0a67da3ceda4a33578021c7b236a36c8c7789a48
TaniaSR8 <20243ds011@utez.edu.mx>
2026-03-24 21:39:21 -0600

    Agrego componente de cerrar sesion

---
+++ frontend/src/components/CerrarSesionModal.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import "../css/CerrarSesionModal.css";
+
+const ModalCerrarSesion = ({ alCancelar, alAceptar }) => {
+  return (
+    <div className="modal-overlay">
+      <div className="logout-card">
+        <h2 className="logout-title">¿Deseas cerrar sesión?</h2>
+        
+        <div className="logout-buttons">
+          <button 
+            className="btn-cancelar" 
+            onClick={alCancelar}
+          >
+            Cancelar
+          </button>
+          
+          <button 
+            className="btn-aceptar" 
+            onClick={alAceptar}
+          >
+            Aceptar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalCerrarSesion;
