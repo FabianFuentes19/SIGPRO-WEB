f2ba7c966a39443ef6450e249309541c446e9bf1
TaniaSR8 <20243ds011@utez.edu.mx>
2026-03-25 23:35:14 -0600

    Subo cambios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
->>>>>>> 3.3.1_front_proyectos
 import AdminRoute from "./components/AdminRoute";
 import DashProyectos from "./pages/DashProyectos";
 import DashLideres from "./pages/DashLideres";
@@ -18,37 +13,34 @@ function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         {/* Redirigir la raíz a login */}
         <Route path="/" element={<Navigate to="/login" />} />
 
         <Route path="/login" element={<Login />} />
-=======
-        
-        <Route path="/" element={<Login />} />
->>>>>>> 3.3.1_front_proyectos
 
         {/* Recuperación de contraseña rutas temporales */}
         <Route path="/recuperar-contraseña" element={<RecuperarContrasena />} />
         <Route path="/restablecer-contraseña" element={<RestablecerContrasena />} />
 
-<<<<<<< HEAD
+        <Route path="/proyectos" element={
+          <AdminRoute>
+            <DashProyectos />
+          </AdminRoute>
+        } />
+
         <Route path="/lideres" element={
           <AdminRoute>
             <DashLideres />
           </AdminRoute>
         } />
 
-        <Route path="/dashboard" element={<Navigate to="/login" />} />
+        <Route path="/dashboard" element={<Navigate to="/dashboard-lider" />} />
         
         <Route path="/dashboard-lider" element={
           <LiderRoute>
             <DashboardLider />
           </LiderRoute>
         } />
-=======
-        
->>>>>>> 3.3.1_front_proyectos
       </Routes>
     </Router>
   );
